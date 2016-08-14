03340513b75471724ae49f9462082cd7dbb51942
Harish Babu <harish_babu@outlook.com>
2016-08-14 17:51:04 +0100

    using the port value from environment variable

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const employers = require('./app/routes/organizations');
 app.use('/timesheet-entries', timesheetEntries);
 app.use('/organizations', employers);
 
-app.listen(5000, () => {
-    console.log('Example app listening on port 5000!');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 });
