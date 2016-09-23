a857ec8d7dca4ed413ed65d586acb4a275112788
Harish Babu <harish_babu@outlook.com>
2016-09-24 00:41:17 +0100

    added stormpath-express package

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var config = require('./config');
+var stormpath = require('express-stormpath');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(expressValidator()); // this line must be immediately after bodyParser.json()!
+app.use(stormpath.init(app, {
+    web: {
+        produces: ['application/json']
+    }
+}))
 
 mongoose.connect(config.mongodb.connectionString);
 
@@ -27,12 +33,14 @@ const offices = require('./app/routes/offices.route');
 
 app.use(cors());
 
-app.use('/timesheet-entries', timesheetEntries);
+app.use('/timesheet-entries', stormpath.apiAuthenticationRequired, timesheetEntries);
 app.use('/organizations', employers);
 app.use('/offices', offices);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+app.on('stormpath.ready', function () {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}!`);
+    });
 });
