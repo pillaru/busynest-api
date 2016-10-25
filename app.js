d478a76d1433f64f8af7811f62c194b5482777e2
Harish Babu <harish_babu@outlook.com>
2016-10-25 22:57:53 +0100

    turned on stormpath logging

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(cors());
 
 app.use(express.static('public'));
 app.use(stormpath.init(app, {
+    debug: 'info',
+    logger: logger,
     web: {
         produces: ['application/json']
     }
@@ -47,4 +49,8 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     logger.info(`app listening on port ${port}!`);
+});
+
+app.on('stormpath.ready', function () {
+  logger.info('Stormpath Ready!');
 });
