e7cccb8f864ece96e6ce99f7ce9016ba10c4645e
Harish Babu <harish_babu@outlook.com>
2017-06-13 00:32:11 +0100

    add assert id on remove unit test

--- tests/providers/mongodb-provider.spec.js
+++ tests/providers/mongodb-provider.spec.js
@@ -1,23 +1,37 @@
+const ObjectID = require('mongodb').ObjectID;
 const MongoDbProvider = require('../../src/providers/mongodb-provider');
 
 describe('the MongoDbProvider module', () => {
+    const collectionStub = {
+        id: '',
+        remove(filter, options, cb) {
+            this.id = filter._id;
+            cb();
+        }
+    };
+    const dbStub = {
+        collectionStub,
+        name: '',
+        collection(name) {
+            this.collectionName = name;
+            return this.collectionStub;
+        }
+    };
+
     test('remove calls collection with passed in collection name', () => {
-        const collectionStub = {
-            remove(filter, options, cb) {
-                cb();
-            }
-        };
-        const dbStub = {
-            name: '',
-            collection(name) {
-                this.collectionName = name;
-                return collectionStub;
-            }
-        };
         const sut = new MongoDbProvider('foo');
         return sut.remove(dbStub, '59172d619e43b10ebeb90e4b')
         .then(() => {
             expect(dbStub.collectionName).toEqual('foo');
+        });
+    });
+
+    test('remove calls remove on collection with id', () => {
+        const id = '59172d619e43b10ebeb90e4b';
+        const sut = new MongoDbProvider('foo');
+        return sut.remove(dbStub, id)
+        .then(() => {
+            expect(dbStub.collectionStub.id).toEqual(new ObjectID(id));
         });
     });
 });
